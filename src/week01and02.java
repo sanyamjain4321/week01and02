d22b521fba654cc5f7c61e8f3b90e333e80be7eb
sanyamjain4321 <sanyamjainofficial4@gmail.com>
2026-03-04 23:12:50 +0530

    P2:Inventory checker

--- src/week01and02.java
+++ src/week01and02.java
@@ -4,66 +4,44 @@ public class week01and02 {
 
     public static void main(String[] args) {
 
-        UsernameChecker checker = new UsernameChecker();
+        InventoryManager manager = new InventoryManager();
 
-        checker.registerUser("john_doe", 1);
+        manager.addProduct("IPHONE15_256GB", 3);
 
-        System.out.println("Is john_doe available? " + checker.checkAvailability("john_doe"));
-        System.out.println("Is jane_smith available? " + checker.checkAvailability("jane_smith"));
+        System.out.println(manager.checkStock("IPHONE15_256GB"));
 
-        System.out.println("Suggestions: " + checker.suggestAlternatives("john_doe"));
-
-        System.out.println("Most attempted username: " + checker.getMostAttempted());
+        System.out.println(manager.purchaseItem("IPHONE15_256GB", 101));
+        System.out.println(manager.purchaseItem("IPHONE15_256GB", 102));
+        System.out.println(manager.purchaseItem("IPHONE15_256GB", 103));
+        System.out.println(manager.purchaseItem("IPHONE15_256GB", 104));
     }
 }
 
-class UsernameChecker {
+class InventoryManager {
 
-    private Map<String, Integer> usernameMap = new HashMap<>();
-    private Map<String, Integer> attemptFrequency = new HashMap<>();
+    private Map<String, Integer> stock = new HashMap<>();
+    private Map<String, Queue<Integer>> waitingList = new HashMap<>();
 
-    public boolean checkAvailability(String username) {
-
-        attemptFrequency.put(username,
-                attemptFrequency.getOrDefault(username, 0) + 1);
-
-        return !usernameMap.containsKey(username);
+    public void addProduct(String productId, int quantity) {
+        stock.put(productId, quantity);
+        waitingList.put(productId, new LinkedList<>());
     }
 
-    public void registerUser(String username, int userId) {
-        usernameMap.put(username, userId);
-    }
+    public synchronized String purchaseItem(String productId, int userId) {
 
-    public List<String> suggestAlternatives(String username) {
+        int current = stock.getOrDefault(productId, 0);
 
-        List<String> suggestions = new ArrayList<>();
-
-        for (int i = 1; i <= 3; i++) {
-            String alt = username + i;
-
-            if (!usernameMap.containsKey(alt)) {
-                suggestions.add(alt);
-            }
+        if (current > 0) {
+            stock.put(productId, current - 1);
+            return "Purchase successful. Remaining: " + (current - 1);
+        } else {
+            Queue<Integer> queue = waitingList.get(productId);
+            queue.add(userId);
+            return "Added to waiting list. Position: " + queue.size();
         }
-
-        suggestions.add(username.replace("_", "."));
-
-        return suggestions;
     }
 
-    public String getMostAttempted() {
-
-        String maxUser = null;
-        int max = 0;
-
-        for (Map.Entry<String, Integer> entry : attemptFrequency.entrySet()) {
-
-            if (entry.getValue() > max) {
-                max = entry.getValue();
-                maxUser = entry.getKey();
-            }
-        }
-
-        return maxUser;
+    public int checkStock(String productId) {
+        return stock.getOrDefault(productId, 0);
     }
 }
