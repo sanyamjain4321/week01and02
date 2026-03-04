32a0b6ee265d46f909d93e243cbb7ebd28c0b371
sanyamjain4321 <sanyamjainofficial4@gmail.com>
2026-03-04 23:15:17 +0530

    P3:DNS Cache with TTL

--- src/week01and02.java
+++ src/week01and02.java
@@ -4,44 +4,49 @@ public class week01and02 {
 
     public static void main(String[] args) {
 
-        InventoryManager manager = new InventoryManager();
+        DNSCache cache = new DNSCache();
 
-        manager.addProduct("IPHONE15_256GB", 3);
-
-        System.out.println(manager.checkStock("IPHONE15_256GB"));
-
-        System.out.println(manager.purchaseItem("IPHONE15_256GB", 101));
-        System.out.println(manager.purchaseItem("IPHONE15_256GB", 102));
-        System.out.println(manager.purchaseItem("IPHONE15_256GB", 103));
-        System.out.println(manager.purchaseItem("IPHONE15_256GB", 104));
+        System.out.println(cache.resolve("google.com"));
+        System.out.println(cache.resolve("google.com"));
     }
 }
 
-class InventoryManager {
+class DNSEntry {
 
-    private Map<String, Integer> stock = new HashMap<>();
-    private Map<String, Queue<Integer>> waitingList = new HashMap<>();
+    String ip;
+    long expiry;
 
-    public void addProduct(String productId, int quantity) {
-        stock.put(productId, quantity);
-        waitingList.put(productId, new LinkedList<>());
+    DNSEntry(String ip, int ttl) {
+        this.ip = ip;
+        this.expiry = System.currentTimeMillis() + ttl * 1000;
     }
 
-    public synchronized String purchaseItem(String productId, int userId) {
+    boolean expired() {
+        return System.currentTimeMillis() > expiry;
+    }
+}
 
-        int current = stock.getOrDefault(productId, 0);
+class DNSCache {
 
-        if (current > 0) {
-            stock.put(productId, current - 1);
-            return "Purchase successful. Remaining: " + (current - 1);
-        } else {
-            Queue<Integer> queue = waitingList.get(productId);
-            queue.add(userId);
-            return "Added to waiting list. Position: " + queue.size();
+    private Map<String, DNSEntry> cache = new HashMap<>();
+
+    public String resolve(String domain) {
+
+        if (cache.containsKey(domain)) {
+
+            DNSEntry entry = cache.get(domain);
+
+            if (!entry.expired()) {
+                return entry.ip;
+            }
+
+            cache.remove(domain);
         }
-    }
 
-    public int checkStock(String productId) {
-        return stock.getOrDefault(productId, 0);
+        String ip = "172.217.14." + new Random().nextInt(255);
+
+        cache.put(domain, new DNSEntry(ip, 300));
+
+        return ip;
     }
 }
