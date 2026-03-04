b44bb4ffde1d1afe1cfb76bcbafecf0991b2b57b
sanyamjain4321 <sanyamjainofficial4@gmail.com>
2026-03-04 23:08:11 +0530

    P1:SOCIAL MEDIA USERNAME CHECKER

--- src/week01and02.java
+++ src/week01and02.java
@@ -1,11 +1,29 @@
 import java.util.*;
 
+public class week01and02 {
+
+    public static void main(String[] args) {
+
+        UsernameChecker checker = new UsernameChecker();
+
+        checker.registerUser("john_doe", 1);
+
+        System.out.println("Is john_doe available? " + checker.checkAvailability("john_doe"));
+        System.out.println("Is jane_smith available? " + checker.checkAvailability("jane_smith"));
+
+        System.out.println("Suggestions: " + checker.suggestAlternatives("john_doe"));
+
+        System.out.println("Most attempted username: " + checker.getMostAttempted());
+    }
+}
+
 class UsernameChecker {
 
     private Map<String, Integer> usernameMap = new HashMap<>();
     private Map<String, Integer> attemptFrequency = new HashMap<>();
 
     public boolean checkAvailability(String username) {
+
         attemptFrequency.put(username,
                 attemptFrequency.getOrDefault(username, 0) + 1);
 
@@ -17,12 +35,15 @@ class UsernameChecker {
     }
 
     public List<String> suggestAlternatives(String username) {
+
         List<String> suggestions = new ArrayList<>();
 
         for (int i = 1; i <= 3; i++) {
             String alt = username + i;
-            if (!usernameMap.containsKey(alt))
+
+            if (!usernameMap.containsKey(alt)) {
                 suggestions.add(alt);
+            }
         }
 
         suggestions.add(username.replace("_", "."));
@@ -31,10 +52,12 @@ class UsernameChecker {
     }
 
     public String getMostAttempted() {
+
         String maxUser = null;
         int max = 0;
 
-        for (var entry : attemptFrequency.entrySet()) {
+        for (Map.Entry<String, Integer> entry : attemptFrequency.entrySet()) {
+
             if (entry.getValue() > max) {
                 max = entry.getValue();
                 maxUser = entry.getKey();
